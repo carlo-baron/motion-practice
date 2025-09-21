1c731a645b3c3d5f46391fbae8eedc94b6aa00fe
carlo-baron <carlobaron430@gmail.com>
2025-09-21 21:24:03 +0800

    Its expandable but im not satisfied, ill improve tom

--- app/(pages)/faq/page.tsx
+++ app/(pages)/faq/page.tsx
@@ -1,3 +1,50 @@
 "use client";
 
-import { AnimatePresence } from 'motion/react';
+import { AnimatePresence, motion } from 'motion/react';
+import { useState } from 'react';
+
+export default function Main(){
+    return(
+        <div className="flex flex-col w-full h-screen items-center">
+            <ExpandingCard>
+                Hello, kamusta
+            </ExpandingCard>
+            <ExpandingCard>
+                <ExpandingCard>
+                </ExpandingCard>
+            </ExpandingCard>
+        </div>
+    );
+}
+
+function ExpandingCard({children=null} : {children?: React.ReactNode}){
+    const [expanded, setExpanded] = useState<boolean>(false);
+
+    return(
+        <div className="w-[80%]">
+            <div className="p-4 h-16 bg-blue-500 flex justify-between rounded-lg">
+                <p className="text-xl font-semibold">Title goes here.</p>
+                <button 
+                    className="cursor-pointer"
+                    onClick={() => setExpanded(!expanded)}
+                >
+                [{expanded ? "Collapse" : "Expand"}]</button>
+            </div>
+            <AnimatePresence>
+                {expanded ? 
+                        <motion.div 
+                        className="rounded-lg h-40 w-full bg-green-500"
+                        initial={{height: "0rem"}}
+                        animate={{height: "10rem"}}
+                        exit={{height: "0rem"}}
+                        transition={{type: "ease", duration: 0.4}}
+                        >
+                            {children}
+                        </motion.div>
+                    :
+                        null
+                }
+            </AnimatePresence>
+        </div>
+    );
+}
